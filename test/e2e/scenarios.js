8d991e4708cfa730109a099493bb13e358b0d6f9
schukanov <schukanov@it.ru>
2016-12-12 10:35:51 +0300

    gulp sequential call

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,11 +2,15 @@
 
 describe('my app', function () {
 
-        browser.get('/');
+
+
+        browser.get('http://localhost:8000');
 
         it('soooo', function () {
-            var todoListItems = element.all(by.repeater('item in list'));
-            expect(todoListItems.count()).toBe(4);
+            //var elements = element.all(protractor.By.css('.clearfix .col206'));
+
+            expect(element.all(by.repeater('item in list')).count()).toEqual(3);
+
         });
 
 
